19c95ffd353f548c083f12286ab97690813d1eff
JungWKim <lionmini@naver.com>
2023-04-17 20:21:03 +0900

    .

---
+++ add_node.sh
@@ -0,0 +1,66 @@
+#---------------
+# 1. run without sudo
+# 2. you need nfs-server for uyuni-infra
+#---------------
+
+#!/bin/bash
+
+cd ~
+
+# disable firewall
+sudo systemctl stop ufw
+sudo systemctl disable ufw
+
+# install basic packages
+sudo apt install -y nfs-common whois
+
+# network configuration
+sudo modprobe overlay \
+    && sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee -a /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee -a /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sudo sysctl --system
+
+# nvidia-container-toolkit configuration
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+   "default-runtime": "nvidia",
+   "runtimes": {
+      "nvidia": {
+            "path": "/usr/bin/nvidia-container-runtime",
+            "runtimeArgs": []
+      }
+   }
+}
+EOF
+
+systemctl restart docker
+sleep 30
+
+# enable kubectl & kubeadm auto-completion
+echo "source <(kubectl completion bash)" >> ${HOME}/.bashrc
+echo "source <(kubeadm completion bash)" >> ${HOME}/.bashrc
+
+echo "source <(kubectl completion bash)" | sudo tee -a /root/.bashrc
+echo "source <(kubeadm completion bash)" | sudo tee -a /root/.bashrc
+
+# install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# install helmfile
+wget https://github.com/helmfile/helmfile/releases/download/v0.150.0/helmfile_0.150.0_linux_amd64.tar.gz
+tar -zxvf helmfile_0.150.0_linux_amd64.tar.gz
+sudo mv helmfile /usr/bin/
+rm LICENSE && rm README.md && rm helmfile_0.150.0_linux_amd64.tar.gz
